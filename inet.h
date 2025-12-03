7c570d7c735db88578b60296ad0e3243fe6bb6d1
noe3 <noe3@viper.cs.ksu.edu>
2025-12-03 16:18:42 -0600

    New Certs

--- inet.h
+++ inet.h
@@ -3,4 +3,4 @@
 #define SERV_TCP_PORT 49158
 
 /* Change the following to be your host addr: 129.130.10.43 for viper and 129.130.10.39 for cougar */
-#define SERV_HOST_ADDR "192.168.1.94"
+#define SERV_HOST_ADDR "129.130.10.43"
